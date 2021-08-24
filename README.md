e7bd6ed987b8fe2f5062916a4896d0ee8f9d9760
root <root@production-builder.us-west1-b.c.railway-infra.internal>
2021-08-24 12:23:02 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Insta Download Bot
+
+This Bot To Download Video/Photo From Instagram.
+
+* Bot Link:  <a href="https://t.me/igbijabot" alt="Ig Bija Bog"> <img src="https://img.shields.io/badge/%F0%9F%A4%96%20-IgBija-yellow" /> </a>
+* Bot channel: <a  href="https://t.me/Nekozu" alt="Nekozu Network"> <img  src="https://img.shields.io/badge/%F0%9F%92%A1-Nekozu-9cf" /> </a>
+
+This bot is free to use. You can change as you like and you are free to do what you want
+
+# Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Nekozu/instagram-Downloader.git)
---
+++ index.js
@@ -0,0 +1,63 @@
+const { Telegraf, Markup } = require('telegraf');
+const axios = require('axios')
+
+const bot = new Telegraf(process.env.token)
+
+let pais = 'Tester'
+
+bot.command('start', (ctx) => {
+  return ctx.replyWithPhoto({ url: 'https://telegra.ph/file/c3f19e89e109e1534b02a.jpg' },
+    {
+      caption: 'Hai '+ctx.from.first_name+' Aku adalah bot untuk mendownload video/photo dari instagram, silahkan ketik /igdl lalu tempelkan link, reels juga bisa loh!.',
+      parse_mode: 'Markdown',
+      ...Markup.inlineKeyboard([
+        Markup.button.url('Subs Channel Bot', 't.me/nekozu'),
+      ])
+    }
+  )
+})
+
+bot.action('help', ctx => {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+    ctx.reply('/igdl -> untuk mendownload foto/video dari instagram\n')
+})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+
+bot.command('igdl', async (ctx) => {
+    let input = ctx.message.text
+    let inputArray = input.split(" ")
+    let message = "";
+    
+    if(inputArray.length == 1){
+        message = "Sertakan Link!" 
+        ctx.reply(message)
+    } else{
+        inputArray.shift();
+        messager = inputArray.join(" ")
+        // console.log(messager)
+        // try{
+        const link = await axios.get(`https://pencarikode.xyz/api/ig?url=`+messager+`&apikey=`+pais)
+        const result = link.data.result.data
+        // const hasill = result
+        // console.log(result)
+        result.forEach((res) => {
+            
+        // console.log(hasil)
+        if(res.type == 'image'){
+        ctx.replyWithPhoto({url: res.data})
+
+        } else {
+            console.log('video')
+            ctx.replyWithVideo({url: res.data})
+        }
+        
+        })
+        // }catch(e){
+        // ctx.reply(`Link not found / wrong link!`)
+        // }
+    }
+})
+
+console.log('Bot Running')
+console.log('Happy Using! Dont Forget To Subs @nekozu!!')
+
+//ctx.reply(`err`)
+bot.launch()
